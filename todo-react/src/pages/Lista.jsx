548bb4c94f3897735d6782dfbdc1686f9e89f629
Nai-star <riosnaidelyn11@gmail.com>
2025-09-08 10:50:44 -0600

    Primera actualización

---
+++ todo-react/src/pages/Lista.jsx
@@ -0,0 +1,11 @@
+import ListaTareas from '../components/ListaTareas'
+
+function Lista() {
+  return (
+    <div>
+     <ListaTareas/>
+    </div>
+  )
+}
+
+export default Lista
---
+++ todo-react/src/routes/Routing.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Lista from '../pages/Lista'
+
+const Routing =() =>{
+  return (
+    <Router>
+        <Routes>
+            <Route path="/Lista" element={<Lista/>}/>
+
+        </Routes>
+    </Router>
+  )
+}
+
+export default Routing
